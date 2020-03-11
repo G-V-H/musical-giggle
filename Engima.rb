cae3857ab32ebddcf1894609d892b45cdcf083fd
GARY <gary.v.ho@gmail.com>
2020-03-12 01:41:42 +1100

    File Cleanup

--- Engima.rb
+++ Engima.rb
@@ -1,15 +1,15 @@
 abc_keys = ("A".."Z").to_a
 
 reflector_values = "EJMZALYXVBWFCRQUONTSPIKHGD".split("")
-reflector_hash = abc_keys.zip(reflector_values)
+REFLECTOR_HASH = abc_keys.zip(reflector_values).freeze
 
-ROTOR1_VALUES = "EJMZALYXVBWFCRQUONTSPIKHGD".split("")
-ROTOR2_VALUES = "YRUHQSLDPXNGOKMIEBFZCWVJAT".split("")
-ROTOR3_VALUES = "FVPJIAOYEDRZXWGCTKUQSBNMHL".split("")
+ROTOR1_VALUES = "EJMZALYXVBWFCRQUONTSPIKHGD".split("").freeze
+ROTOR2_VALUES = "YRUHQSLDPXNGOKMIEBFZCWVJAT".split("").freeze
+ROTOR3_VALUES = "FVPJIAOYEDRZXWGCTKUQSBNMHL".split("").freeze
 
 rotor1_hash = Hash[abc_keys.zip(ROTOR1_VALUES)]
 rotor2_hash = Hash[abc_keys.zip(ROTOR2_VALUES)]
-rotor3_hash = Hash[abc_keys.zip(rotor3_values)]
+rotor3_hash = Hash[abc_keys.zip(ROTOR3_VALUES)]
 
 
 def input(r1, r2, r3)
@@ -18,11 +18,9 @@ def input(r1, r2, r3)
         c = r1[c]
         c = r2[c]
         c = r3[c]
-        # p c
+        p c
     }
 
 end
 
-# input(rotor1_hash, rotor2_hash, rotor3_hash)
-
-p 26 % 25 * 25
+input(rotor1_hash, rotor2_hash, rotor3_hash)
