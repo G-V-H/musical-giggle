c0cfcaff8cde5dba101eeed144b49a0626bdd1ff
GARY <gary.v.ho@gmail.com>
2020-03-12 02:54:48 +1100

    defaults added

--- Engima.rb
+++ Engima.rb
@@ -1,24 +1,26 @@
-ABC_KEYS = ("A".."Z").to_a.freeze
+ABC_KEYS = ("A".."Z").to_a
 
 reflector_values = "EJMZALYXVBWFCRQUONTSPIKHGD".split("")
-REFLECTOR_HASH = Hash[ABC_KEYS.zip(reflector_values)].freeze
+REFLECTOR_HASH = Hash[ABC_KEYS.zip(reflector_values)]
 
-ROTOR1_VALUES = "EJMZALYXVBWFCRQUONTSPIKHGD".split("").freeze
-ROTOR2_VALUES = "YRUHQSLDPXNGOKMIEBFZCWVJAT".split("").freeze
-ROTOR3_VALUES = "FVPJIAOYEDRZXWGCTKUQSBNMHL".split("").freeze
+ROTOR1_VALUES = "EJMZALYXVBWFCRQUONTSPIKHGD".split("")
+ROTOR2_VALUES = "YRUHQSLDPXNGOKMIEBFZCWVJAT".split("")
+ROTOR3_VALUES = "FVPJIAOYEDRZXWGCTKUQSBNMHL".split("")
 
-
-# rotor2_hash = Hash[ABC_KEYS.zip(ROTOR2_VALUES)]
-# rotor3_hash = Hash[ABC_KEYS.zip(ROTOR3_VALUES)]
+DEFAULT_ROTOR1 = Hash[ABC_KEYS.zip(ROTOR1_VALUES)]
+DEFAULT_ROTOR2 = Hash[ABC_KEYS.zip(ROTOR2_VALUES)]
+DEFAULT_ROTOR3 = Hash[ABC_KEYS.zip(ROTOR3_VALUES)]
 
 
 def input()
+    str = "abcdefghihjk"
 
-    str = "str"
-    r1 = ROTOR1_VALUES
-    r2 = ROTOR2_VALUES
-    r3 = ROTOR3_VALUES
-    str.upcase.each_char.with_index {|c, i| 
+    r1 = DEFAULT_ROTOR1.dup
+    r2 = DEFAULT_ROTOR2.dup
+    r3 = DEFAULT_ROTOR3.dup
+
+    str.upcase.each_char.with_index.map {|c, i| 
+
         r1 = rotate_rotor(ROTOR1_VALUES, i)
         if i % 26 == 25
             r2 = rotate_rotor(ROTOR2_VALUES, (i % 25))
@@ -26,7 +28,6 @@ def input()
         if i % 676 == 0
             r3 = rotate_rotor(ROTOR3_VALUES, (i % 675))
         end
-        p c
         c = r1[c]
         c = r2[c]
         c = r3[c]
@@ -37,13 +38,17 @@ def input()
         c = r2.invert[c]
         c = r1.invert[c]
 
-        p c
-    }
+        
+    }.join
 
+end
+
+def rotor_setting(rotor)
+    
 end
 
 def rotate_rotor(rotor, i)
-    rotor = Hash[ABC_KEYS.rotate(i).zip(ROTOR1_VALUES.rotate(i))]
+    Hash[ABC_KEYS.zip(rotor.rotate(i))]
 end
 
-input()
+p input()
